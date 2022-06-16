34f9813c460fc8528a8b6a7da13a9b58b75cff93
billwautraining <107490658+billwautraining@users.noreply.github.com>
2022-06-16 15:19:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)                      
        
-streamlit.header("Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlist.text_input('What fruit would you like information about?', 'Kiwi')
 streamlist.write('The user entered', fruit_choice)
 
