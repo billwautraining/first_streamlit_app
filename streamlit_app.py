4be078cafd6aa076150c051265ea0e55cc78132a
billwautraining <107490658+billwautraining@users.noreply.github.com>
2022-06-16 15:19:33 -0400

    Update streamlit_app.py
    added user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)                      
-                      
+       
+streamlit.header("Fruityvice Fruit Advice!')
+fruit_choice = streamlist.text_input('What fruit would you like information about?', 'Kiwi')
+streamlist.write('The user entered', fruit_choice)
+
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
